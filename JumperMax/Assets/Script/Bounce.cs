c5470aeb190f659b368491cf9335531d68007297
Fatih Gayberi <46620361+fatihgayberi@users.noreply.github.com>
2020-07-05 23:15:14 +0300

    player control
    player control

---
+++ JumperMax/Assets/Script/Bounce.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bounce : MonoBehaviour
+{
+    public float jumpPower = 15f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        Rigidbody2D rb2d = collision.collider.GetComponent<Rigidbody2D>();
+
+        if (rb2d != null)
+        {
+            Vector2 velocity = rb2d.velocity;
+            velocity.y = jumpPower;
+            rb2d.velocity = velocity;
+        }
+    }
+}
