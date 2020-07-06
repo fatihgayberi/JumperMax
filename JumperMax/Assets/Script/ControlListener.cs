13bfb0c67a7d1b8350006ac2e8a64d46267c9d81
Fatih Gayberi <46620361+fatihgayberi@users.noreply.github.com>
2020-07-06 03:41:34 +0300

    Player Control
    Player Control

---
+++ JumperMax/Assets/Script/ControlListener.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControlListener : MonoBehaviour
+{
+    public GameObject player;
+    private float num = -1f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        Rigidbody2D rb2d = collision.collider.GetComponent<Rigidbody2D>();
+
+        if (rb2d.name == "Player")
+        {
+            player.transform.position = new Vector3(num * player.transform.position.x, player.transform.position.y, player.transform.position.z);
+        }
+
+        
+    }
+}
