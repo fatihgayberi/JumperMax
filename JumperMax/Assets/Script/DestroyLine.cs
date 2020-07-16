dba62f8762e6f17d40d4fb6109253fe3d0c5448d
Fatih Gayberi <46620361+fatihgayberi@users.noreply.github.com>
2020-07-17 00:13:44 +0300

    general
    general

--- JumperMax/Assets/Script/DestroyLine.cs
+++ JumperMax/Assets/Script/DestroyLine.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class DestroyLine : MonoBehaviour
 {
+    public string nm;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,15 +14,24 @@ public class DestroyLine : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        Debug.Log("update: " + nm);
     }
-
-    void OnTriggerEnter2D (Collider2D collision)
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-        //Rigidbody2D rb2d = collision.collider.GetComponent<Rigidbody2D>();
-
-        Debug.Log("name:" + collision.gameObject.name);
-
-
+        if (collision.gameObject.CompareTag("NormalPlatform"))
+        {
+            Debug.Log("hello ammar");
+        }
+        nm = collision.gameObject.name;
+        Debug.Log("name1:" + collision.gameObject.name);
+        Debug.Log("name2:" + gameObject.name);
     }
+    //void OnTriggerEnter2D (Collider2D collision)
+    //{
+    //    //Rigidbody2D rb2d = collision.collider.GetComponent<Rigidbody2D>();
+
+    //    Debug.Log("name:" + collision.gameObject.name);
+
+
+    //}
 }
